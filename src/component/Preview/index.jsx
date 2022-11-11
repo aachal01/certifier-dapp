60aad839a7827c9fe068cf6c5ab4b53fea562649
Aachal Modak <95211796+aachal01@users.noreply.github.com>
2022-11-11 14:29:16 +0530

    create certificate

---
+++ src/component/Preview/index.jsx
@@ -0,0 +1,7 @@
+import './styles.scss';
+import {ReactComponent as Template} from '../../assets/template.png'
+import { Link } from 'react-router-dom';
+
+export function Preview({ verify, create }) {
+  
+}
--- src/pages/create/index.jsx
+++ src/pages/create/index.jsx
@@ -1,12 +1,43 @@
 import './styles.scss';
 import { SideNav } from '../../component/SideNav';
+import { Preview } from '../../component/Preview';
 
 export function CreatePage() {
     return (
         <div className='create-page'>
+            {/* Sidenav */}
             <SideNav verify={true}/>
-            <div className='editor'>For Editor</div>
-            <div className='preview'>For Preview</div>
+
+            {/* Certificate editor */}
+            <div className='editor'>
+                    <div className="form">
+                        <h3>Create Certificate</h3>
+                        <label className="input-box">
+                            <span className="details">Title</span>
+                            <input type="text" placeholder="Enter Title"/>
+                        </label>
+                        <label className="input-box">
+                            <span className="details">Name</span>
+                            <input type="text" placeholder="Enter your Name"  />
+                        </label>
+                        <label className="input-box">
+                            <span className="details">Issued Date</span>
+                            <input type="date" placeholder="DD-MM-YYYY"  />
+                        </label>
+                        <label className="input-box">
+                            <span className="details">Expiry Date</span>
+                            <input type="date" placeholder="DD-MM-YYYY"  />
+                        </label>
+                        <label className="input-box">
+                            <span className="details">Description</span>
+                            <div className='des-container' placeholder="Enter Description" contentEditable/>
+                        </label>
+                        <button>Submit</button>
+                    </div>
+            </div>
+
+            {/* Certificate preview */}
+            <div className='preview'> <Preview/> </div>
         </div>
     )
 }
